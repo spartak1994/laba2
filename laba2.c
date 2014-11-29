767c2652ba0fa79c65ad00e5d2ae865076f0771a
spartak1994 <spartak1994@mail.ru>
2014-11-29 23:30:39 +0400

    second commit

--- laba2.c
+++ laba2.c
@@ -23,40 +23,25 @@ int count_of_process=pow(2, time->tm_hour);
 for(i=0;i<count_of_process;i++)
 {
 
-printf("\ni=%d\n", i);
-pid=fork();
+	printf("\ni=%d\n", i);
+	pid=fork();
 
-if(pid==0)
-{
-getTime(&time);
-printf("pid = %d, getpid=%d, getppid=%d, sin = %f\n",pid, getpid(), getppid(), sin(3.1415*(time->tm_min)/14)); 
-exit(0);
-}
-else {
-printf("pid = %d\n", pid);
-if (pid==-1) printf("Error\n"); 
-if (pid&1)
-kill(pid, SIGKILL);
-died=wait(&status);
-printf("Died with %d status\n", died);
+	if(pid==0)
+	{
+		getTime(&time);
+		sin(3.1415*(time->tm_min)/14); 
+		exit(0);
+	}
+	else {
+	printf("pid = %d\n", pid);
+	if (pid==-1) printf("Error\n"); 
+	if (pid&1)
+	died=wait(&status);
+	printf("Exit with %d status\n", died);
 //printf("Process PID, PPID and status %d, %d, %d\n", getpid(), getppid(), died);
 }
 
-/*switch(pid)
-	{
-		case -1: 
-printf("can't fork\n");
-exit(-1);
-		case 0:	
-getTime(&time);
-sin(3.1415*(time->tm_min)/15); 
-exit(0);
-		default: 
-if (pid&1)
-kill(pid, SIGKILL);
-died=wait(&status);
-printf("Process PID, PPID and status %d, %d, %d\n", getpid(), getppid(), died);
-	}	*/		
+		
 }
 
 
